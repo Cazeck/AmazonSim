f6e0fa6fad9b13ef006f609e236dbda635ab5689
Cazeck <caseykolodziejczyk@gmail.com>
2020-03-06 16:45:45 -0600

    Added shipPackage to DockArea

--- DockArea.py
+++ DockArea.py
@@ -6,3 +6,11 @@ class DockArea:
 
     def __init__(self, pointlist):
         self.points = pointlist
+
+    def shipPackage(self, belt, package):
+        beltcell = belt.getBeltLocation()       # Location of Belt at DockArea
+        belt.removeObject(package)              # Remove Package from Belt
+        beltcell.removeContent(package)         # Remove Package from Cell
+        print(f'Package has been shipped to {package.destination}')
+        del package                             # Delete Package from Memory
+        print('Package Deleted')
