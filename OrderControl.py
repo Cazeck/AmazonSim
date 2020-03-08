9af150b9cffb23ec3ccebb7804eb0449650f0812
Cazeck <caseykolodziejczyk@gmail.com>
2020-03-07 19:34:31 -0600

    Added genRandomOrder
    Can now create random order instances for testing

--- OrderControl.py
+++ OrderControl.py
@@ -1,18 +1,18 @@
+import random
 from Order import Order
+
 
 # Going to function similarly to RobotScheduler but handling orders instead
 
-
 class OrderControl:
 
-    # Should order status' be listed here or within Orders class?
-
-    def __init__(self, env):
+    def __init__(self, env, inv):
         self.clock = env
+        self.inventory = inv
         self.allOrders = []     # a List of orders, dictionary might be necessary
         self.currentOrder = None
 
-        self.populate()
+        #self.populate()
 
     def addOrder(self, order):
         # a check to see if there is a duplicate order?
@@ -35,8 +35,34 @@ class OrderControl:
     def getCurrentOrder(self):
         return self.currentOrder
 
-    #
-    #def complete
+    # Creates a Random Order objects with Items from Catalog
+    def genRandomOrder(self):
+        order_items = []
+        order_address = self.randomAddress()
+        # Choose a random number between 2 - 3 (# of Items)
+        order_size = random.randrange(2, 4)
+        # Choose a random Item from Catalog 2 - 3 times
+        for i in range(0, order_size):
+            item = random.choice(self.inventory.catalog.getItemList())
+            order_items.append(item)
+
+        # Create Order instance
+        new_order = Order(order_items, order_address)
+        # Add Order to Queue
+        self.addOrder(new_order)
+        #return new_order
+
+    # Generate random address for an Order
+    def randomAddress(self):
+        address_number = random.randrange(1, 500)
+        street_names = ['First', 'Second', 'Third', 'Park', 'Main', 'Oak', 'Pine', 'Maple', 'Cedar', 'Elm', 'Lake']
+        address_name = random.choice(street_names)
+        address_endings = ['Avenue', 'Court', 'Lane', 'Parkway', 'Road', 'Street', 'Trail']
+        address_street = random.choice(address_endings)
+
+        order_address = str(address_number) + ' ' + str(address_name) + ' ' + str(address_street)
+
+        return order_address
 
     def populate(self):
         # Create some order instances   #Add Bagpipe bad to order1
