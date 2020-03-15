2f7a25b53baf896f61945304fbc004ecc58c0595
Cazeck <caseykolodziejczyk@gmail.com>
2020-03-15 03:32:38 -0500

    Updated Documentation

--- Robot.py
+++ Robot.py
@@ -1,133 +1,153 @@
-from Point import Point
 
 class Robot:
+    """
+        Robot class represents the robots that move around the warehouse and move shelves from place to place
 
-    numofrobots = 0
-    # Stages for where the robot is while processing an order
-    # status will use these values
-    idle = 0
-    pickershelfbound = 1
-    pickerbound = 2
-    atpicker = 3
-    shelfhomebound = 4
-    shelfhomelocation = 5
-    chargerbound = 6
-    atcharger = 7
+        Class Variables:
+        num_of_robots: A integer used to count the total number of shelves created
 
-    def __init__(self, name, startinglocation):
+        Attributes:
+            robot_name: A string representing the name of the robot
+            location: Point object that is the current location of the robot
+            cell: Cell object that is the robots current location on the Floor and display
+            charging: A boolean indicating if the robot is at a charging location
+            holding_shelf: A Shelf object that is being carried by the robot
+            destination: A list of Point objects that is the path of steps it will take to get to its destination
+
+        """
+
+    num_of_robots = 0
+
+    def __init__(self, name, starting_location):
+        """
+        Inits Robot with a name and a starting location
+
+        Args:
+            name: A string to be used as the name of the Robot (Ex: Robot A)
+            starting_location: A Point object marking the robots current location
+
+        """
+        self.robot_name = name
+        self.location = starting_location
         self.cell = None
-        self.location = startinglocation
-        self.status = 0             # start off idle
-        self.destination = None       # Will be a point object List of points? Next point needed to go you 1 by 1
-        self.holdingShelf = None    # Number of shelf that it is holding
-        self.shelflocation = None   # Location of the shelf that it needs to get / return
-        self.picker = None
-        self.dock = None
-        self.robotName = name
+        self.charging = True            # Is True because robots start on a charging station
+        self.holding_shelf = None
+        self.destination = None
+        Robot.num_of_robots += 1
 
-        Robot.numofrobots += 1
-
-    # Return where the robot needs to go
     def getDestination(self):
+        """
+        Returns the path that the robot needs to take to get to its destination
+
+        Returns:
+            destination: A list of Points
+        """
         return self.destination
 
     def setCell(self, cell):
+        """
+        Changes the Cell that Robot is located at to cell and changes Robot's location
+
+        Args:
+            cell: A Cell object that the robot's location will be changed to
+        """
         self.cell = cell
         self.setLocation(cell.cellLocation())
 
     def getCell(self):
+        """
+        Returns the Cell that the robot is located at
+
+        Returns:
+            The Cell object containing this Robot
+        """
         return self.cell
 
-    def  getName(self):
-        return self.robotName
+    def getName(self):
+        """
+         Returns the name of the Robot object
 
-    # Return where the Robot is at
+         Returns:
+             The robot_name of the Robot object
+         """
+        return self.robot_name
+
     def getLocation(self):
+        """
+        Returns the point location of the Robot object
+
+        Returns:
+            The Point object location of the Robot
+        """
         return self.location
 
-    def setLocation(self, newlocation):
-        self.location = newlocation
+    def setLocation(self, new_location):
+        """
+        Changes the Point that Robot is located at to new_location
 
-    # Return the robots current status
-    def getStatus(self):
-        return self.status
+        Args:
+            new_location: A Point object that the robot's location will be changed to
+        """
+        self.location = new_location
 
-    def pickUpShelf(self, shelf):
-        self.holdingShelf = shelf
-        self.holdingShelf.resting = False
+    def pickUpShelf(self, new_shelf):
+        """
+        The Robot "picks up" new_shelf by adding it to holding_shelf
 
-    def putDownShelf(self, shelf):
-        self.holdingShelf.resting = True
-        self.holdingShelf = None
+        Args:
+            new_shelf: A Shelf object which is no longer resting
+        """
+        self.holding_shelf = new_shelf
+        self.holding_shelf.resting = False
 
-    # Return the shelf that it is currently holding
+    def putDownShelf(self, shelf_held):
+        """
+        The Robot "puts down" shelf_held by removing it from holding_shelf
+
+        Args:
+            shelf_held: A Shelf object which now resting
+        """
+        self.holding_shelf.resting = True
+        self.holding_shelf = None
+
     def getHoldingShelf(self):
-        return self.holdingShelf
+        """
+        Returns the Shelf that robot is currently holding
 
-    # Change status for Robot
-    def setState(self, newstate):
-        self.status = newstate
+        Returns:
+            The holding_shelf of the Robot object
+        """
+        return self.holding_shelf
 
-    # Give the Robot a destination to go to
-    # Destination will be a list of Points to follow
-    def setDestination(self, destination):
-        self.destination = destination
+    def setDestination(self, new_destination):
+        """
+        Changes the path that Robot will follow to a destination
 
-    # Method will be used by RobotScheduler every tick
-    # to move the robot by one point
-    # Might not need this if RobotScheduler one works better
-    # CAN DELETE ONCE SIMULATION IS FIGURED OUT
-    #def moveByOne(self):
-        # if holding a shelf
-        #if self.destination != [] and self.getholdingShelf() is not None:
-        #    # If holding a shelf, we need to also change the cells for the shelf as we move each tick
-        #    self.location = self.destination[0]
-        #    self.getholdingShelf().resting = False
+        Args:
+            new_destination: A list of Point objects
+        """
+        self.destination = new_destination
 
-        # if moving by itself
-     #   if self.destination != []:
-
-            # Need to change the contents of the Cell we are in and the next
-            #print(f'current Cell is: {self.cell}')
-            #print(f'next Cell is: {self.destination[0]}')
-
-            #print("| Moving from: " + str(self.location) + " To: " + str(self.destination[0]))
-      #      self.location = self.destination[0]     # Get next point in the list
-       #     del self.destination[0]                 # Remove first point in the list
-
-
-
-
-
-        #else:
-       #     print("Robot is already at destination!")
-
-    # Will tell the robot to start heading to a new destination
-    def goToDest(self):
-        #self.destination = newDest
-        self.moveByOne()
-
-
-    # Umambiguous reperestation of object, used for debugging / loggin - seen for developers
-    # Item('Bong', 67090, 4)
     def __repr__(self):
-        return "Robot('{}', '{}', {}, {})".format(self.robotName, self.status, self.location, self.holdingShelf)
+        """
+        Unambiguous representation of Robot object
 
-    # readable representation of an object, used for display to end users
-    # Item: Hydras Lament - Serial No: 11008 - Shelf: None
+        Used for debugging and logging
+
+        Returns:
+            In the format: Robot(robot_name, location, holding_shelf)
+            Ex: Robot("C", Point(5, 2), Shelf 4)
+        """
+        return "Robot('{}', {}, {})".format(self.robot_name, self.location, self.holding_shelf)
+
     def __str__(self):
-        return '| Robot: {} -  Status: {} - Location: {}  Shelf Held: {}'.format(self.robotName, self.status, self.location, self.holdingShelf)
+        """
+        Readable representation of Robot object
 
+        Used for display
 
-
-#startPoint1 = Point(1,2)
-
-#startPoint2 = Point(7,8)
-
-#roboA= Robot('A', startPoint1)
-
-#roboB = Robot('B', startPoint2)
-
-#print(roboA)
-
-#print(roboB)
+        Returns:
+            In the format: Robot: "C" - Location: Point(5, 2) - Shelf: 4
+        """
+        return 'Robot: {} - Location: {} - Shelf Held: {}'.format(self.robot_name, self.location,
+                                                                  self.holding_shelf)
