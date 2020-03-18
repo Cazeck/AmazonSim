38722130f42c91f8f9121cebf2099ec22b58bc91
Cazeck <caseykolodziejczyk@gmail.com>
2020-03-18 18:35:26 -0500

    Updated Documentation

--- Cell.py
+++ Cell.py
@@ -14,46 +14,101 @@ from DockArea import DockArea
 from Bin import Bin
 
 class Cell(Point):
+    """
+    Cell class is an extension of Point that enables a coordinate location to contain objects
 
-    # What is currently in this cell, could be either a Robot or Shelf
-    #content = None
+    Each Cell is one square location on the Floor of the warehouse.  This enables us to keep
+    track of how objects are interacting with each other as orders are fulfilled. It is also
+    very useful for visualizing and displaying the simulation
+
+    Attributes:
+        x: Integer representing x coordinate
+        y: Integer representing y coordinate
+        content: List that will contain an Object if one is at the same Point as Cell
+    """
 
     def __init__(self, point):
+        """
+        Inits Cell with x and y values from a specified Point
+
+        Cells contain nothing until something is placed inside of it
+
+        Args:
+            point: Point object with an x and y coordinate
+        """
         self.x = point.x
         self.y = point.y
         self.content = []
 
-    # Returns location of Cell (Point Object)
     def cellLocation(self):
+        """
+        Returns the location of the Cell as a Point object
+
+        Returns:
+            New Point object that is the Cell's x and y location
+        """
         return Point(self.x, self.y)
 
-    # Returns whats currently in the cell
     def getContents(self):
-        if len(self.content) == 1:
-            #if isinstance(self.content[0], DockArea):   # Specifically for Dock Area being scuffed right now
-                #return self.content
-            #else:
-            return self.content
+        """
+        Returns whats currently within the Cell
 
-        if len(self.content) == 2:
-            return self.content
+        Returns:
+            List of objects within Cell
+        """
+        return self.content
+        # Something was scuffed here earlier, keeping this for the time being
+        #if len(self.content) == 1:
+        #    return self.content
+
+        #if len(self.content) == 2:
+        #    return self.content
 
         # should be empty List at this point
-        return self.content
+        #return self.content
 
-    # Adds an object to the cell
-    def setContents(self, object):
-        self.content.append(object)
+    def setContents(self, new_object):
+        """
+        Adds an object into the Cell
 
-    def removeContent(self, object):
-        self.content.remove(object)
+        Args:
+            new_object: The object that will now be located at at this Cell
+        """
+        self.content.append(new_object)
+
+    def removeContent(self, object_to_remove):
+        """
+        Removes an object from the Cell
+
+        Args:
+            object_to_remove: The object that will no longer be at this Cell
+        """
+        self.content.remove(object_to_remove)
 
     def __repr__(self):
+        """
+        Unambiguous representation of Shelf object
+
+        Used for debugging and logging
+
+        Returns:
+            In the format: Cell(x, y, content)
+            Ex: Cell(7, 3, [object])
+        """
         return "Cell('{}', '{}', {})".format(self.x, self.y, self.content)
 
     def __str__(self):
-        #result = super.__str__(self)
+        """
+        Readable representation of Shelf object
 
+        Used for display
+        More Complicated than the rest because this was initally used for creating a
+        print-out display of the warehouse floor.  Added a bunch of specific cases to
+        help display the variety of objects that could be within a Cell in a clear format
+
+        Returns:
+            In the format: Cell x:1 y:1 contains Object
+        """
         result = "Cell x:{} y:{}".format(self.x, self.y)
 
         if len(self.content) == 1:
@@ -88,40 +143,25 @@ class Cell(Point):
 
         if len(self.content) == 2:
 
-            if (isinstance(self.content[0], Robot) and isinstance(self.content[1], Shelf)) or (isinstance(self.content[1], Robot) and isinstance(self.content[0], Shelf)):
+            if (isinstance(self.content[0], Robot) and isinstance(self.content[1], Shelf))\
+                    or (isinstance(self.content[1], Robot) and isinstance(self.content[0], Shelf)):
                 result += " contains Robot and Shelf"
                 return result
 
-            if (isinstance(self.content[0], Robot) and isinstance(self.content[1], Charger)) or (isinstance(self.content[1], Robot) and isinstance(self.content[0], Charger)):
+            if (isinstance(self.content[0], Robot) and isinstance(self.content[1], Charger))\
+                    or (isinstance(self.content[1], Robot) and isinstance(self.content[0], Charger)):
                 result += " contains Robot and Charger"
                 return result
 
-            if (isinstance(self.content[0], Belt) and isinstance(self.content[1], Bin)) or (isinstance(self.content[1], Belt) and isinstance(self.content[0], Bin)):
+            if (isinstance(self.content[0], Belt) and isinstance(self.content[1], Bin))\
+                    or (isinstance(self.content[1], Belt) and isinstance(self.content[0], Bin)):
                 result += " contains Belt and Bin"
                 return result
 
-            if (isinstance(self.content[0], Belt) and isinstance(self.content[1], Package)) or (isinstance(self.content[1], Belt) and isinstance(self.content[0], Package)):
+            if (isinstance(self.content[0], Belt) and isinstance(self.content[1], Package))\
+                    or (isinstance(self.content[1], Belt) and isinstance(self.content[0], Package)):
                 result += " contains Belt and Package"
                 return result
 
-
         result += " contains Nothing"
         return result
-        # Might need a clone funct for visualizer
-
-
-#point1 = Point(5,3)
-
-#point2 = Point(5,2)
-
-#cell1 = Cell(point1)
-
-#shelf1 = Shelf(1, 10)
-#print(point1)
-
-#print(point2.below())
-#cell1.setContents(shelf1)
-#print(cell1.content)
-#print(cell1.x)
-
-#print(cell1)
--- Point.py
+++ Point.py
@@ -1,74 +1,61 @@
 
-# Point is used to establish coordinates in the shelf area
-
-
-# x = Right/Left , y = Up/Down
 class Point:
+    """
+    Point class is used to establish coordinate locations throughout the warehouse
 
-    # Class Variables, applies to all instances
-    areasizeX = 10      # Width of Area (East)
-    areasizeY = 10      # Height of Area (North)
+    These points allow us to have an understanding how objects are moving around during the simulation
+
+    Attributes:
+        x: Integer representing x coordinate
+        y: Integer representing y coordinate
+    """
 
     def __init__(self, x, y):
+        """
+        Inits Point with x and y values
+
+        Args:
+            x: Integer of x location
+            y: Integer of y location
+        """
         self.x = x
         self.y = y
 
-    # Return the point just "right" of this point (= East)
-    # or return none if there is no "right"
-    def right(self):
-        newx = self.x + 1
-        if newx > self.areasizeX:
-            print("out of bounds")
-            return None
-        return Point(newx, self.y)
+    # Not in use
+    def equals(self, other_point):
+        """
+        Compares the current point object with another to see if they are in the same location
 
-    # Return the point just "left" of this point (= West)
-     # or return none if there is no "left"
-    def below(self):
-        newx = self.y - 1
-        if newx < 0:
-            print("out of bounds")
-            return None
-        return Point(newx, self.y)
+        Args:
+            other_point: Point object to compare location with
 
-    # Return the point just "above" this point (= North)
-    # or return none if there is no "above"
-    def above(self):
-        newy = self.y + 1
-        if newy > self.areasizeY:
-            print("out of bounds")
-            return None
-        return Point(self.x, newy)
+                Returns:
+            is_same: Boolean determining whether or not the Points are the same position
+        """
 
-    # Return the point just "below" this point (= South)
-    # or return none if there is no "below"
-    def below(self):
-        newy = self.y - 1
-        if newy < 0:
-            print("out of bounds")
-            return None
-        return Point(self.x, newy)
-
-
-    # Compares two points against each other to see if they are equal
-    def equals(self, otherPoint):
-        issame = self.x == otherPoint.x and self.y == otherPoint.y
-        return issame
+        is_same = self.x == other_point.x and self.y == other_point.y
+        return is_same
 
     def __repr__(self):
+        """
+        Unambiguous representation of Shelf object
+
+        Used for debugging and logging
+
+        Returns:
+            In the format: Point(x, y)
+            Ex: Point(1, 2)
+        """
         return "Point('{}', '{}')".format(self.x, self.y)
 
     def __str__(self):
+        """
+        Readable representation of Shelf object
+
+        Used for display
+
+        Returns:
+            In the format:
+            Point x:1 y:2
+        """
         return "Point x:{} y:{}".format(self.x, self.y)
-
-
-
-#point1 = Point(5,3)
-
-#point2 = Point(5,2)
-#print(point1)
-#print(point2)
-
-#print(point1.equals(point2))
-
-#print(point1.above())
